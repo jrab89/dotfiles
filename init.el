2f722d0fcce6c98bae75e00ffba04073fd19bf49
Jeff Rabovsky <jeffr@jellyvision.com>
2016-01-05 23:44:40 -0600

    moved web-mode

--- init.el
+++ init.el
@@ -41,25 +41,6 @@
 ;; (load-theme 'solarized-dark t)
 (load-theme 'solarized-light t)
 
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.json\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.xml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
-(setq web-mode-markup-indent-offset 2)
-(setq web-mode-code-indent-offset 2)
-(setq web-mode-enable-current-element-highlight t)
-(eval-after-load "web-mode" ;; M-x list-colors-display !!!
-  '(set-face-background 'web-mode-current-element-highlight-face "light green"))
-
 ;;------------------------------------------------------------------------------
 ;; UI
 
