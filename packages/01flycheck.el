eea83471794cc6e3fa90f2c43ae937329ad814d9
Jeff Rabovsky <jeffr@jellyvision.com>
2016-02-21 22:42:53 -0600

    ruby-lint is annoying

--- packages/01flycheck.el
+++ packages/01flycheck.el
@@ -3,7 +3,7 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (setq flycheck-display-errors-delay 0.1)
 (with-eval-after-load 'flycheck
-  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc ruby-lint)))
 
 (eval-after-load 'flycheck
   '(add-hook 'flycheck-mode-hook #'flycheck-typescript-tslint-setup))
