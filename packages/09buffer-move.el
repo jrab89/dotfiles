f1c9dce31dedfc458121b549e7571434970ab2f1
Jeff Rabovsky <jrab89@gmail.com>
2016-09-09 22:37:20 -0500

    need to disable some OS X shortcuts for the buffer-move suggested key bindings to work

--- packages/09buffer-move.el
+++ packages/09buffer-move.el
@@ -1,4 +1,7 @@
 (require 'buffer-move)
+
+;; up/down need "Mission Control" and"Application windows" disabled in:
+;; "System Preferences" -> "Mission Control" -> "Keyboard and Mouse Shortcuts"
 (global-set-key (kbd "<C-S-up>")     'buf-move-up)
 (global-set-key (kbd "<C-S-down>")   'buf-move-down)
 (global-set-key (kbd "<C-S-left>")   'buf-move-left)
