900f9affcbbafd730e25a28203a709d0a67f695d
Jeff Rabovsky <jrab89@gmail.com>
2017-10-06 10:22:35 -0500

    - Replaced ido, neotree, find-file-in-project, and smex with helm, projectile, and helm-projectile - Removed a bunch of stuff I don't use on a regular basis: clojure/cider, go, typescript/tide - using zenburn-theme instead of solarized

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,30 +4,22 @@
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa-stable" . "https://stable.melpa.org/packages/")))
 
-(defvar my-packages '(ag
-                      auto-highlight-symbol
+(defvar my-packages '(auto-highlight-symbol
                       better-defaults
-                      cider
                       company
-                      company-go
                       company-jedi
                       exec-path-from-shell
-                      find-file-in-project
                       flycheck
                       git-gutter
-                      go-eldoc
-                      go-mode
-                      ido-ubiquitous
-                      ido-vertical-mode
+                      helm
+                      helm-projectile
                       json-mode
                       markdown-mode
-                      neotree
-                      smex
-                      solarized-theme
+                      projectile
                       terraform-mode
-                      tide
                       windresize
                       yaml-mode
+                      zenburn-theme
                       zygospore))
 
 (package-initialize)
@@ -41,11 +33,26 @@
 (dolist (file-name (directory-files "~/.emacs.d/vendor" nil "\\.el$"))
   (load file-name))
 
-(load-theme 'solarized-dark t)
+(load-theme 'zenburn t)
+
+;; helm
+(require 'helm-config)
+(require 'helm)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+
+;; projectile
+(require 'projectile)
+(projectile-global-mode)
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
+(global-set-key (kbd "C-c c") 'helm-projectile)
 
 ;; auto-highlight-symbol
 (require 'auto-highlight-symbol)
-(setq ahs-modes (append ahs-modes '(go-mode)))
+(setq ahs-modes (append ahs-modes '()))
 (global-auto-highlight-symbol-mode t)
 (setq ahs-idle-interval 0.1)
 
@@ -57,10 +64,11 @@
 
 ;; exec-path-from-shell
 (exec-path-from-shell-initialize)
-(exec-path-from-shell-copy-env "PYTHONPATH")
-(exec-path-from-shell-copy-env "GOPATH")
 
 ;; python
+
+;; https://github.com/tkf/emacs-jedi/issues/178#issuecomment-315958663
+(setq jedi:server-args '("--virtual-env" "/Users/jrabovsky/.virtualenvs/default"))
 (add-hook 'python-mode-hook
           (lambda ()
             (add-to-list 'company-backends 'company-jedi)))
@@ -116,49 +124,6 @@
 (unless (server-running-p)
   (server-start))
 
-;; neotree
-(setq neo-smart-open t)
-(setq neo-keymap-style 'concise)
-(setq neo-vc-integration '(face char))
-(setq neo-autorefresh nil) ;; neotree 0.5.2 will constantly change root if this is non-nil
-(require 'neotree)
-(add-to-list 'neo-hidden-regexp-list "__pycache__")
-(add-to-list 'neo-hidden-regexp-list ".egg-info")
-
-(defun neotree-opened-in-other-frame-p ()
-    (and (neo-global--get-window)
-         (not (eq (window-frame (neo-global--get-window))
-                  (window-frame (selected-window))))))
-
-(defun neotree-project-root ()
-  "Open NeoTree using ffip-project-root"
-  (interactive)
-  (when (neotree-opened-in-other-frame-p)
-    (neotree-hide))
-  (let ((project-dir (ffip-project-root))
-        (file-name (buffer-file-name)))
-    (if project-dir
-        (progn
-          (neotree-dir project-dir)
-          (neotree-find file-name))
-      (message "ffip couldn't find the project root."))))
-
-;; ido
-(ido-mode 1)
-(ido-everywhere 1)
-(ido-ubiquitous-mode 1)
-(ido-vertical-mode 1)
-(setq ido-vertical-show-count 1)
-(setq ido-vertical-define-keys 'C-n-C-p-up-down-left-right)
-
-;; smex
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command) ;; old M-x.
-
-;; find-file-in-project
-(setq ffip-prefer-ido-mode t)
-
 ;; git-gutter
 (global-git-gutter-mode t)
 (git-gutter:linum-setup)
@@ -170,49 +135,12 @@
 
 ;; eldoc
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'python-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'go-mode-hook 'go-eldoc-setup)
 (setq eldoc-idle-delay 0.2)
-
-;; go
-(require 'go-mode)
-(require 'company)
-(require 'company-go)
-(setq gofmt-command "goimports")
-(add-hook 'before-save-hook 'gofmt-before-save)
-(add-hook 'go-mode-hook (lambda ()
-                          (set (make-local-variable 'company-backends) '(company-go))
-                          (company-mode)))
-(add-hook 'go-mode-hook
-          (lambda ()
-            (setq tab-width 4)))
-
-(setq company-go-show-annotation t)
-(setq company-tooltip-align-annotations t)
 
 ;; zygospore
 (require 'zygospore)
-(defvar zygospore-reopen-neotree-p nil)
-
-(defun zygospore-should-restore-p ()
-  (and (equal (selected-window) (next-window))
-       (equal (selected-window) zygospore-last-full-frame-window)
-       (equal (current-buffer) zygospore-last-full-frame-buffer)))
-
-(global-set-key (kbd "C-x 1")
-                (lambda ()
-                  (interactive)
-                  (if (zygospore-should-restore-p)
-                      (progn
-                        (zygospore-restore-other-windows)
-                        (when zygospore-reopen-neotree-p
-                          (neotree-show)
-                          (setq zygospore-reopen-neotree-p nil)))
-                    (when (neo-global--window-exists-p)
-                      (neotree-hide)
-                      (setq zygospore-reopen-neotree-p t))
-                    (zygospore-delete-other-window))))
+(global-set-key (kbd "C-x 1") 'zygospore-toggle-delete-other-windows)
 
 ;; term
 (add-hook 'term-mode-hook
@@ -224,23 +152,14 @@
 ;; http://stackoverflow.com/a/36344479
 (with-eval-after-load "term"
   (define-key term-raw-map (kbd "C-y") 'term-paste)
-  (define-key term-raw-map (kbd "M-x") 'smex)
   (define-key term-raw-map (kbd "s-v") 'term-paste))
-
-;; TypeScript
-(defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-  (company-mode +1))
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
 
 ;; ibuffer
 (setq ibuffer-default-sorting-mode 'filename/process)
 
 ;; flycheck
+(require 'flycheck)
+(setq-default flycheck-disabled-checkers '(python-pylint emacs-lisp-checkdoc))
 (global-flycheck-mode)
 
 ;; makes the big dumb warning sign go away in emacs 25
@@ -253,9 +172,9 @@
 
 ;; TODO
 ;; ruby
-;; python virtualenv
+;; better python virtualenv support?
 ;; sessions/desktop
 ;; magit
 ;; which-key
 ;; elscreen
-;; projectile and/or helm
+;; dumb jump
