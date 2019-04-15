2b85a4ce327b83e1554e8d253ee5c0f6112be32d
Jeff Rabovsky <jeff.rabovsky@centro.net>
2019-04-15 14:16:10 -0500

    fixed checksum failure output

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eu
 
-mitamae_version="1.7.5"
-mitamae_darwin_sha256="89d6de16fe1398281c1d46c28274d3ab14203f41605f9f5c9863773fc0af9ef8"
+mitamae_version='1.7.5'
+mitamae_darwin_sha256='89d6de16fe1398281c1d46c28274d3ab14203f41605f9f5c9863773fc0af9ef8'
 script_dir=$(dirname "$0")
 
 if ! [ -f "${script_dir}/mitamae" ]; then
@@ -9,7 +9,7 @@ if ! [ -f "${script_dir}/mitamae" ]; then
     actual_sha256="$(/usr/bin/openssl dgst -sha256 "/tmp/mitamae" | cut -d" " -f2)"
 
     if [ "$mitamae_darwin_sha256" != "$actual_sha256" ]; then
-        echo "checksum verification failed! \n expected: ${mitamae_darwin_sha256} \n actual: ${actual_sha256}"
+        echo 'checksum verification failed!'
         echo "  expected: ${mitamae_darwin_sha256}"
         echo "  actual:   ${actual_sha256}"
         exit 1
